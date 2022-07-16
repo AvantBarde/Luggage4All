0f62896d2f79932f1244543850d16f4aa45f4add
Bardell <Bardellwilson7@gmail.com>
2022-07-16 11:29:29 -0500

    working on db/index.js

--- db/index.js
+++ db/index.js
@@ -1,7 +1,37 @@
 const client = require('./client');
 const models = require('./models');
 
+const {
+  getOrderProductById,
+  addProductToOrder,
+  updateOrderProduct,
+  destroyOrderProduct
+} = require('./order_products.js');
+const {
+  getOrderById,
+  getAllOrders,
+  getOrdersByUser,
+  getOrdersByProduct,
+  getCartByUser,
+  createOrder,
+} = require('./orders.js')
+const {
+  getProductById,
+  getAllProducts,
+  createProduct,
+} = require('./products.js')
+const {
+  getAllUsers,
+  createUser,
+  getUser,
+  getUserById,
+  getUserByUsername,
+} = require('./user.js');
+
 module.exports = {
   client,
-  ...models,
+  ...require("./products"),
+  ...require("./order_products"),
+  ...require("./users"),
+  ...require("./orders")
 };
