5de49cb5b131c3d427418b3e077306b1643b7c21
jordan <jordansteger95@gmail.com>
2022-07-19 19:06:52 -0500

    card component added in components folder

--- src/components/product_card.js
+++ src/components/product_card.js
@@ -1,17 +1,27 @@
 import React from "react";
+import "./product_card.css";
 
-import React from 'react'
-
-function product_card({title, imageUrl, body}) {
+function product_card({ title, imageUrl, body }) {
   return (
     <div className="card-container">
-        <div className="image-container">
-            <img src={imageUrl} alt='' />        
+      <div className="image-container">
+        <img src={imageUrl} alt="" />
+      </div>
+      <div className="card-content">
+        <div className="card-title">
+          <h3> {title} </h3>
         </div>
+        <div className="card-body">
+          <p> {body} </p>
+        </div>
+        <div className="btn">
+          <button>
+            <a>View product</a>
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default product_card;
-
-
