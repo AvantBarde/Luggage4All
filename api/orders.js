ae865bfa61510aef632b8baca071d5b263930148
David Stein <dstein131@gmail.com>
2022-07-22 16:45:12 -0400

    orders api

--- api/orders.js
+++ api/orders.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const ordersRouter = express.Router();
 const { Orders } = require('../db/models')
-const { adminRequired } = require('./utils')
+const { adminRequired, requireUser } = require('./utils')
+const { Cart } = require('../db/models')
 
 // Return a list of orders, include the products with them. requires admin access
 ordersRouter.get('/', adminRequired, async (req, res, next) => {
@@ -13,5 +14,43 @@ ordersRouter.get('/', adminRequired, async (req, res, next) => {
   }
 }
 )
+
+//  GET /orders/cart (*)
+// Return the current user's order with status='created' (synonymous to a 'cart'). Use database adapter getCartByUser
+ordersRouter.get('/cart', requireUser, async (req, res, next) => {
+  try {
+    const cart = await Cart.getCartByUser(req.user.id)
+    res.send(cart)
+  } catch (error) {
+    next(error)
+  }
+}
+)
+
+// POST /orders (*)
+// Create a new order. Should initially be status = created.
+
+ordersRouter.post('/', requireUser, async (req, res, next) => {
+  try {
+    const order = await Orders.createOrder({status: 'created', userId: req.user.id})
+    res.send(order)
+  } catch (error) {
+    next(error)
+  }
+}
+)
+
+// GET /users/:userId/orders (**)
+// Get a list of orders for a particular user.
+ordersRouter.get('/:userId', adminRequired, async (req, res, next) => {
+  try {
+    const orders = await Orders.getOrdersByUser(req.params.userId)
+    res.send(orders)
+  } catch (error) {
+    next(error)
+  }
+}
+)
+
 
 module.exports = ordersRouter
