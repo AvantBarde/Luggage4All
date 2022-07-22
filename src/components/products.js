6d11bb0e271c1821b2ea2b5db0793e6d3c780471
Bardell <Bardellwilson7@gmail.com>
2022-07-22 11:16:30 -0500

    committing to pull

--- src/components/products.js
+++ src/components/products.js
@@ -1,10 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import { useHistory } from 'react-router-dom';
 
-function Products() {
+function Products({setToken}) {
     const [productId, setProductId] = useState(null);
     const [quantity, setQuantity] = useState(1);
-    const token = localStorage.getItem("token");
+    setToken(localStorage.getItem("token"));
 
 
   return (
