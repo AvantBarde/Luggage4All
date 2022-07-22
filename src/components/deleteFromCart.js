204ab9806c5d1bf3ec7ad8067c065d1e09546ec1
David Stein <dstein131@gmail.com>
2022-07-21 20:30:10 -0400

    delete fronm cart

---
+++ src/components/deleteFromCart.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { deleteProductFromCart } from '../../db/models/cart'
+import { Button } from 'react-bootstrap'
+
+function DeleteFromCart(props) {
+
+    const [error, setError] = useState('')
+    const history = useHistory()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const response = await deleteProductFromCart (props.userId, props.productId)
+            if (response) {
+                history.push('/cart')
+            } else {
+                setError('Error deleting from cart')
+            }
+        }
+        catch (error) {
+            console.error(error)
+        }
+    }
+
+
+
+  return (
+    <Button variant="primary" type="submit" onClick={handleSubmit}>
+        Delete from Cart
+    </Button>
+  )
+}
+
+export default DeleteFromCart
