7d2fed92a0ff100f3839c22c93458ab709116fe9
Bardell <Bardellwilson7@gmail.com>
2022-07-23 10:31:59 -0500

    adding the index file back

---
+++ src/axios-services/index.js
@@ -0,0 +1,101 @@
+import axios from 'axios';
+
+// this file holds your frontend network request adapters
+// think about each function as a service that provides data
+// to your React UI through AJAX calls
+
+// for example, if we need to display a list of users
+// we'd probably want to define a getUsers service like this:
+
+/* 
+  export async function getUsers() {
+    try {
+      const { data: users } = await axios.get('/api/users')
+      return users;
+    } catch(err) {
+      console.error(err)
+    }
+  }
+*/
+
+export async function getAPIHealth() {
+  try {
+    const { data } = await axios.get('/api/health');
+    return data;
+  } catch (err) {
+    console.error(err);
+    return { healthy: false };
+  }
+}
+
+export async function getProductCard(id) {
+  try {
+    const { data: product } = await axios.get(`/api/products/${id}`);
+    return product;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export async function getAllProducts() {
+  try {
+    const { data: products } = await axios.get('/api/products');
+    return products;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export async function reqHeaders(token) {
+  return (token ? 
+    {
+        "Content-Type" : "application/json",
+        "Authorization" : `Bearer ${token}`
+    } :
+    {
+        "Content-Type" : "application/json"
+    } )
+  }
+
+export async function tokenLogin( method, username, password, setToken) {
+  fetch(`api/users/login`, {
+    method: method ? method.toUpperCase() : "GET",
+    headers : {
+      "Content-Type" : "application/json"
+    },
+    body: JSON.stringify({
+      user: {
+        username: username,
+        password: password
+      }
+    })
+  }).then(response => response.json())
+    .then(result => {
+      setToken(result.data.token);
+      localStorage.setItem("jwt", result.data.token);
+      alert(result.data.message);
+    })
+    .catch(console.error);
+}
+
+
+export async function tokenRegister( method, username, password, setToken) {
+  fetch(`api/users/register`, {
+    method: method ? method.toUpperCase() : "GET",
+    headers : {
+      "Content-Type" : "application/json"
+    },
+    body: JSON.stringify({
+      user: {
+        username: username,
+        password: password
+      }
+    })
+  }).then(response => response.json())
+    .then(result => {
+      setToken(result.data.token);
+      localStorage.setItem("jwt", result.data.token);
+      alert(result.data.message);
+    })
+    .catch(console.error);
+}
