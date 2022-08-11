02e8a9acbf4075218999c30170066fd7ada05bab
Bardell <Bardellwilson7@gmail.com>
2022-08-11 18:42:44 -0500

    edited client

--- db/client.js
+++ db/client.js
@@ -5,23 +5,32 @@ require ('dotenv').config();
 // change the DB_NAME string to whatever your group decides on
 const DB_NAME = 'grace_shopper';
 
-const DB_URL =
-  process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
+// const DB_URL =
+//   process.env.DATABASE_URL;
 
-let client;
+
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+
 
 // github actions client config
-if (process.env.CI) {
-  client = new Client({
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: 'postgres',
-    database: 'postgres',
-  });
-} else {
+// if (process.env.CI) {
+//   client = new Client({
+//     host: 'localhost',
+//     port: 5432,
+//     user: 'postgres',
+//     password: 'postgres',
+//     database: 'postgres',
+//   });
+// } else {
   // local / heroku client config
-  client = new Client(DB_URL);
-}
+  // client = new Client(DB_URL);
+// }
 
 module.exports = client;
